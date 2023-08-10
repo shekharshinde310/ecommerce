01a4add2414ada7c4222cb47cb58abc243f51649
shekharshinde310 <shekharshinde310@gmail.com>
2023-08-10 20:42:07 +0530

    first commit

--- src/app/shared/service/dataService.service.ts
+++ src/app/shared/service/dataService.service.ts
@@ -7,19 +7,18 @@ export class DataService {
     {
       iName: "Green Apple",
       iPrice: "110",
-      iImg: "https://onedaycart.com/odcb/wp-content/uploads/2016/01/19-Amazing-Benefits-Of-Cumin-Jeera-For-Skin-Hair-And-Health.jpg"
+      iImg: "/assets/imgs/gApple.jpg"
     },
     {
       iName: "Orange",
       iPrice: "100",
-      iImg: "https://m.media-amazon.com/images/I/51AzriIvcBL._SX342_.jpg" 
+      iImg: "/assets/imgs/orange.jpg"
     },
     {
       iName: "Papaya",
       iPrice: "90",
-      iImg: "https://m.media-amazon.com/images/I/51AzriIvcBL._SX342_.jpg"
-      
-  }
+      iImg: "/assets/imgs/papaya.jpg"
+    }
   ];
   private bagArr: any[] = [
     {
@@ -28,7 +27,7 @@ export class DataService {
       qty: 2,
       total: 50,
     }
-  ];  
+  ];
   dataSubject = new BehaviorSubject<any>(this.arr.slice());
   bagDataSubject = new BehaviorSubject<any>(this.bagArr.slice());
 
@@ -40,13 +39,13 @@ export class DataService {
     })
     this.dataSubject.next(this.arr.slice())
   }
-  
+
   receivedBagData(bagObj: any) {
     this.bagArr.push(bagObj)
     this.bagDataSubject.next(this.bagArr.slice())
   }
-  cardIndex(id:number){
-    return this.arr.slice()[id] 
-  } 
+  cardIndex(id: number) {
+    return this.arr.slice()[id]
+  }
 
 }
